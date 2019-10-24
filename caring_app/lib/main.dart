aa9dd9cce457dc6983e866c73d8f01d64962d812
caring-dev-acc <56320623+caring-dev-acc@users.noreply.github.com>
2019-10-24 23:01:52 +0300

    Andrei - Modificari
    Am adaugat navigarea prin TabBar si prin swipe
    Am schimbat icon-ul aplicatiei + titlul

--- caring_app/lib/main.dart
+++ caring_app/lib/main.dart
@@ -9,45 +9,45 @@ import 'screens/Profile.dart';
 import 'login/Authentication.dart';
 import 'login/Firebase.dart';
 
-
 void main() => runApp(MyApp());
 
-Map<int, Color> primaryDarkBlue_map =
-{
-  50:Color.fromRGBO(8,13,24, .1),
-  100:Color.fromRGBO(8,13,24, .2),
-  200:Color.fromRGBO(8,13,24, .3),
-  300:Color.fromRGBO(8,13,24, .4),
-  400:Color.fromRGBO(8,13,24, .5),
-  500:Color.fromRGBO(8,13,24, .6),
-  600:Color.fromRGBO(8,13,24, .7),
-  700:Color.fromRGBO(8,13,24, .8),
-  800:Color.fromRGBO(8,13,24, .9),
-  900:Color.fromRGBO(8,13,24, 1),
+Map<int, Color> primaryDarkBlue_map = {
+  50: Color.fromRGBO(8, 13, 24, .1),
+  100: Color.fromRGBO(8, 13, 24, .2),
+  200: Color.fromRGBO(8, 13, 24, .3),
+  300: Color.fromRGBO(8, 13, 24, .4),
+  400: Color.fromRGBO(8, 13, 24, .5),
+  500: Color.fromRGBO(8, 13, 24, .6),
+  600: Color.fromRGBO(8, 13, 24, .7),
+  700: Color.fromRGBO(8, 13, 24, .8),
+  800: Color.fromRGBO(8, 13, 24, .9),
+  900: Color.fromRGBO(8, 13, 24, 1),
 };
 
 MaterialColor primaryDarkBlue = MaterialColor(0xFF14213d, primaryDarkBlue_map);
 
 Map<int, Color> secondaryPopYellow_map = {
-  50:Color.fromRGBO(99, 64, 7, .1),
-  100:Color.fromRGBO(99, 64, 7, .2),
-  200:Color.fromRGBO(99, 64, 7, .3),
-  300:Color.fromRGBO(99, 64, 7, .4),
-  400:Color.fromRGBO(99, 64, 7, .5),
-  500:Color.fromRGBO(99, 64, 7, .6),
-  600:Color.fromRGBO(99, 64, 7, .7),
-  700:Color.fromRGBO(99, 64, 7, .8),
-  800:Color.fromRGBO(99, 64, 7, .9),
-  900:Color.fromRGBO(99, 64, 7, 1),
+  50: Color.fromRGBO(99, 64, 7, .1),
+  100: Color.fromRGBO(99, 64, 7, .2),
+  200: Color.fromRGBO(99, 64, 7, .3),
+  300: Color.fromRGBO(99, 64, 7, .4),
+  400: Color.fromRGBO(99, 64, 7, .5),
+  500: Color.fromRGBO(99, 64, 7, .6),
+  600: Color.fromRGBO(99, 64, 7, .7),
+  700: Color.fromRGBO(99, 64, 7, .8),
+  800: Color.fromRGBO(99, 64, 7, .9),
+  900: Color.fromRGBO(99, 64, 7, 1),
 };
 
-MaterialColor secondaryPopYellow = MaterialColor(0xFFfca311, secondaryPopYellow_map);
+MaterialColor secondaryPopYellow =
+    MaterialColor(0xFFfca311, secondaryPopYellow_map);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
+      title: 'CARing',
       color: secondaryPopYellow,
       home: DefaultTabController(
         length: 3,
@@ -55,24 +55,30 @@ class MyApp extends StatelessWidget {
           body: TabBarView(
             children: [
               new Container(
-                color: Colors.yellow,
-              ),
-              new Container(color: Colors.orange,),
+                  child: new Scaffold(
+                      body: Center(
+                child: Icon(Icons.battery_charging_full),
+              ))),
               new Container(
-                color: Colors.lightGreen,
-              ),
+                  child: new Scaffold(
+                      body: Center(
+                child: Icon(Icons.chat),
+              ))),
+              new Container(
+                  child: new Scaffold(
+                      body: Center(
+                child: Icon(Icons.textsms),
+              ))),
             ],
           ),
           bottomNavigationBar: new TabBar(
+            isScrollable: false,
             tabs: [
               Tab(
                 icon: new Icon(Icons.account_circle),
                 text: 'Cont',
               ),
-              Tab(
-                icon: new Icon(Icons.message),
-                text: 'Mesaje'
-              ),
+              Tab(icon: new Icon(Icons.message), text: 'Mesaje'),
               Tab(
                 icon: new Icon(Icons.card_giftcard),
                 text: 'Beneficii',
@@ -80,7 +86,7 @@ class MyApp extends StatelessWidget {
             ],
             labelColor: secondaryPopYellow,
             unselectedLabelColor: Colors.white,
-            indicatorSize: TabBarIndicatorSize.label,
+            indicatorSize: TabBarIndicatorSize.tab,
             indicatorPadding: EdgeInsets.all(5.0),
             indicatorColor: secondaryPopYellow,
           ),
