440a7cb3886811427318d64fff24c28c8c5a485c
caring-dev-acc <56320623+caring-dev-acc@users.noreply.github.com>
2019-10-26 13:37:06 +0300

    Update Benefits.dart

---
+++ caring_app/lib/screens/Benefits.dart
@@ -0,0 +1,15 @@
+import 'package:caring_app/main.dart';
+import 'package:flutter/material.dart';
+
+class Benefits extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:
+      Center(
+          child: (Text('testing the widget from another screen')
+          )
+          ),
+        );  
+  }
+}
