a37fb99de7bfe9d45dafb41dd68f1b23c2a9a1cf
John <45206493+JohnUnciuleac@users.noreply.github.com>
2019-10-31 17:48:57 +0200

    ma bate dart v2

--- caring_app/lib/main.dart
+++ caring_app/lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
               new Container(
                   child: new Scaffold(
                       body: Center(
-                child:new Conversations("Contacts screen"),
+                child: new Conversations("Contacts screen"),
               ))),
               new Container(
                   child: new Scaffold(
@@ -78,7 +78,10 @@ class MyApp extends StatelessWidget {
                 icon: new Icon(Icons.account_circle),
                 text: 'Cont',
               ),
-              Tab(icon: new Icon(Icons.message), text: 'Mesaje'),
+              Tab(
+                icon: new Icon(Icons.message), 
+                text: 'Mesaje'
+                ),
               Tab(
                 icon: new Icon(Icons.card_giftcard),
                 text: 'Beneficii',
@@ -94,44 +97,5 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-
-//Page change
-class DashboardScreen extends StatefulWidget {
-  DashboardScreen({Key key}) : super(key: key);
-
-  @override
-  _DashboardScreenState createState() => new _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  PageController _pageController;
-  int _page = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = new PageController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
-
-  void navigationTapped(int page) {
-    // Animating to the page.
-    // You can use whatever duration and curve you like
-    _pageController.animateToPage(page,
-        duration: const Duration(milliseconds: 10), curve: Curves.linear);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      this._page = page;
-    });
   }
 }
