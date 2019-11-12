00e74dab8011f8931d02e50c647e4ad936432aa2
AlexFurdui <45711052+AlexFurdui@users.noreply.github.com>
2019-11-12 12:45:03 +0200

    Revert "sdfgh"
    This reverts commit 8946d15821b139a745763015288340b3442976f8.

--- caring_app/test/widget_test.dart
+++ caring_app/test/widget_test.dart
@@ -7,7 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:CARing/main.dart';
+
+import 'package:caring_app/main.dart';
 
 void main() { 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
