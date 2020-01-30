934d1dbb4788b05df25bf991780e5fa18d57f70b
Andrei Coroeanu <56926148+andrew-c0@users.noreply.github.com>
2020-01-30 22:07:55 +0200

    Update main.dart

--- caring_app/lib/main.dart
+++ caring_app/lib/main.dart
@@ -15,13 +15,36 @@ import 'login/Firebase.dart';
 
 void main() => runApp(MyApp());
 
+Map<int, Color> primaryDarkBlue_map = {
+  50: Color.fromRGBO(8, 13, 24, .1),
+  100: Color.fromRGBO(8, 13, 24, .2),
+  200: Color.fromRGBO(8, 13, 24, .3),
+  300: Color.fromRGBO(8, 13, 24, .4),
+  400: Color.fromRGBO(8, 13, 24, .5),
+  500: Color.fromRGBO(8, 13, 24, .6),
+  600: Color.fromRGBO(8, 13, 24, .7),
+  700: Color.fromRGBO(8, 13, 24, .8),
+  800: Color.fromRGBO(8, 13, 24, .9),
+  900: Color.fromRGBO(8, 13, 24, 1),
 };
 
 MaterialColor primaryDarkBlue = MaterialColor(0xFF14213d, primaryDarkBlue_map);
 
 Map<int, Color> secondaryPopYellow_map = {
+  50: Color.fromRGBO(99, 64, 7, .1),
+  100: Color.fromRGBO(99, 64, 7, .2),
+  200: Color.fromRGBO(99, 64, 7, .3),
+  300: Color.fromRGBO(99, 64, 7, .4),
+  400: Color.fromRGBO(99, 64, 7, .5),
+  500: Color.fromRGBO(99, 64, 7, .6),
+  600: Color.fromRGBO(99, 64, 7, .7),
+  700: Color.fromRGBO(99, 64, 7, .8),
+  800: Color.fromRGBO(99, 64, 7, .9),
+  900: Color.fromRGBO(99, 64, 7, 1),
 };
 
+MaterialColor secondaryPopYellow =
+    MaterialColor(0xFFfca311, secondaryPopYellow_map);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
